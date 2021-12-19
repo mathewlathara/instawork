2fd00225272bfd1383236b156ce5682da042fa13
mathew <mathutti@gmail.com>
2021-12-19 18:58:28 -0500

    Implementing a model into the app

--- instawork/instaworkapp/models.py
+++ instawork/instaworkapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):  
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
